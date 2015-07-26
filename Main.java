14584439925dec04971f488faf68f0fc6954ad32
Interwebnet <wneuffer@iupui.edu>
2015-07-26 14:27:46 -0400

    Junctions.  Barely.
    Needs work, I think it is the synchronization... But i am not sure...
    Could be a few things... but basically it works about 50/50.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
+
 
 import javax.sound.sampled.LineUnavailableException;
 
@@ -11,7 +11,7 @@ public class Main {
 		
 		String sMusicLoc = "/piano/piano-ff-0";
 		ArrayList<SoundPlayer> playerList = new ArrayList<SoundPlayer>();
-		int channelNum=3;
+		int channelNum=30;
 		boolean[] bRelease = new boolean[channelNum];
 		double shouldBeHigh = 0;
 		double[] iFileNum = new double[channelNum];
@@ -19,19 +19,19 @@ public class Main {
 		SoundPlayer soundLoader = new SoundPlayer();
 		soundLoader.loadSound(sMusicLoc);	
 		double firstNote = 0;
+		int count = 0;
+		int tempNum  =1;
 		for(int i = 0;i<channelNum;i++){
 			SoundPlayer soundTest = new SoundPlayer();	
 			playerList.add(soundTest);
-			System.out.println(playerList.size());
-			
-			
+			//System.out.println(playerList.size());	
 		}
 		iFileNum[0]= System.currentTimeMillis();
 		for(int i = 0;i<channelNum;i++){
 			iFileNum[i] = iFileNum[0] +((1550/channelNum)*i+1);
 		}
-
-		for(int iPlayNum = 0;iPlayNum<=30;){
+		
+		for(int iPlayNum = 1;iPlayNum<=59;){
 			//this area needs work.  A lot of work.
 			for(int i=0;i<channelNum;i++){
 				shouldBeHigh++;
@@ -49,8 +49,18 @@ public class Main {
 					if (bRelease[i]){
 						bRelease[i]=false;
 						System.out.println("thread: " + i + ", time:"+ iFileNum[i]+", PlayNum:" + iPlayNum);
-						iPlayNum++;
-						iFileNum[i]=iFileNum[i]+2;
+						if (count ==channelNum){
+							count =0;
+							tempNum++;
+							iPlayNum=tempNum;
+							
+						}
+						else{
+							iPlayNum++;
+							count++;
+						}
+						
+
 					}
 				}
 			}
--- SoundPlayer.java
+++ SoundPlayer.java
@@ -1,28 +1,27 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+
 import java.util.concurrent.Semaphore;
 
 import javax.sound.sampled.*;
 
 public class SoundPlayer  {
 	private Semaphore mutex;
-	private Clip clip;
+//	private Clip clip;
 	static private ArrayList<AudioInputStream> audioList = new ArrayList<AudioInputStream>();
-	
-
+	static private ArrayList<Semaphore> mutexList = new ArrayList<Semaphore>();
+	static private Semaphore closer = new Semaphore(1);
 	public SoundPlayer(){
 		mutex = new Semaphore(1);
-		try {
-			clip = AudioSystem.getClip();
-		}catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+//		try {
+//			clip = AudioSystem.getClip();
+//		}catch (Exception e) {
+//			System.err.println(e.getMessage());
+//		}
 	}
-	public boolean isNotActive(){
-		return (!clip.isActive());
-	}
+//	public boolean isNotActive(){
+//		return (!clip.isActive());
+//	}
 	public boolean isLocked(){
 		return (mutex.availablePermits()< 1);
 	}
@@ -32,12 +31,19 @@ public class SoundPlayer  {
 				try {
 					System.out.println(args+ " asdasdasd");
 					if(args == "/piano/piano-ff-0"){
-						for(int i = 30;i<=60;i++){
-						System.out.println(args +i);
+						String temp;
+						for(int i = 1;i<=60;i++){
+							if(i<10){
+								temp="0";
+							}
+							else temp ="";
+							Semaphore mutex = new Semaphore(1);
+							System.out.println(args+ temp+i);
 							AudioInputStream inputStream;
 							inputStream = AudioSystem.getAudioInputStream(		    
-									SoundPlayer.class.getResourceAsStream(args+i+".wav"));
+									SoundPlayer.class.getResourceAsStream(args+temp+i+".wav"));
 							audioList.add(inputStream);
+							mutexList.add(mutex);
 							//System.out.println(audioList.size() + " asdasdasd");
 							}
 					}
@@ -55,55 +61,92 @@ public class SoundPlayer  {
 	
 		class Player implements Runnable {
 			public void run() {
-				
-				//System.out.println(audioList.size()+ " "+ args +" " + clip.isActive());
-				try {
-				//	System.out.println(mutex.availablePermits() + " MUTEX");
-
-					clip.open(audioList.get(args));
+				synchronized(mutexList.get(args)){
+					Clip clip = null;
 					
-				} catch (LineUnavailableException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-//					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-//					gainControl.setValue(0.0f);
-				
-				clip.start(); 
-				double time= System.currentTimeMillis();
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				clip.drain();
-				System.out.println("Playing: "+args+", in thread.");
-				while(clip.isActive()){
-					clip.drain();
-				}
-				double time2= System.currentTimeMillis();
-				System.out.println(time2 - time + " playing:" + args);
-				
-				clip.stop();
-				clip.close();
-				
-				mutex.release();
-	
-			}
-		}
+					try {
+						clip = AudioSystem.getClip();
+					} catch (LineUnavailableException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					//System.out.println(audioList.size()+ " "+ args +" " + clip.isActive());
+					try {
+					//	System.out.println(mutex.availablePermits() + " MUTEX");
+						//mutexList.get(args).acquire();
+						
+						clip.open(audioList.get(args));
+					} catch (LineUnavailableException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+	//					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+	//					gainControl.setValue(0.0f);
 
+
+					
+					while(closer.availablePermits()>0){
+						
+					
+						clip.setMicrosecondPosition(0);
+						clip.start(); 
+						double time= System.currentTimeMillis();
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						clip.drain();
+						//System.out.println("Playing: "+args+", in thread.");
+						while(clip.isActive()){
+							clip.drain();
+						}
+						double time2= System.currentTimeMillis();
+						//System.out.println(time2 - time + " playing:" + args + " Length: "+audioList.get(args).getFrameLength()+" Object: "+audioList.get(args));
+		
+						
+						clip.stop();
+						mutex.release();
+						try {
+							mutexList.get(args).wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						
+					}
+					//System.out.println(clip.getLongFramePosition()+"LINE");
+					clip.close();
+					
+					
+					}
+				}
+			}
+		
 		if(audioList.size() > args & mutex.availablePermits()>0){
-			//System.out.println(mutex.availablePermits()+ " Permits");
+			if(mutexList.get(args).availablePermits()>0){
+			//System.out.println(mutexList.get(args).availablePermits()+ " Permits");
 			mutex.acquire();
+			mutexList.get(args).acquire();
 			Player player = new Player();
 			Thread play = new Thread(player);
+			play.setName("Thread-"+args);
 			play.start();
-
 			return true;
+			}
+			else{
+				//System.out.println("In here "+ args);
+				mutex.acquire();
+				synchronized(mutexList.get(args)){
+					mutexList.get(args).notify();
+					return true;
+				}
+			}
 		}
 		
 
@@ -112,6 +155,7 @@ public class SoundPlayer  {
 		return false;
 		
 	}
+	
 }	
 
 
